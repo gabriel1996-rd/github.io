879eaf788634b6d42c2ee782840539722cb1322e
gabo966 <gabo.lira96@gmail.com>
2025-06-28 14:26:59 -0400

    Primera parte practica

---
+++ Practica Javascript/Contenido_Javascript.js
@@ -0,0 +1,14 @@
+const persona = {};
+console.log("Objeto inicial",persona);
+
+// Propiades agregadas
+persona.nombre= "Gabriel";
+persona.sexo="Masculino";
+persona.ocupacion="Estudiante";
+console.log("Objeto luego de agregar propiedades",persona);
+
+//Delete
+delete persona.sexo
+console.log("Objeto despues del delete",persona);
+
+
